b881740557c87bde3d082dc13ca5512689edaa5c
cedlemo <cedlemo@gmx.com>
2016-11-07 19:56:42 +0100

    Continue working on sample

--- sample/walk.rb
+++ sample/walk.rb
@@ -5,10 +5,27 @@ require "#{PATH}/../lib/ggit.rb"
 
 repo_path = "#{PATH}/ruby-gnome2/.git"
 file = Gio::File.path(repo_path)
-#begin
-  Ggit::Repository.open(file)
-#rescue => error
-#  puts error.message
-#end
-#revwalker = Ggit::RevisionWalker.new(repo)
+repo = Ggit::Repository.open(file)
+revwalker = Ggit::RevisionWalker.new(repo)
+revwalker.sort_mode = [:time, :topological]
+head = repo.head
+revwalker.push(head.target)
+while oid = revwalker.next do
+  commit = repo.lookup(oid, Ggit::Commit.gtype)
+  author = commit.author
+  puts author.name
+  puts author.time.format("%c")
+  break
+  puts commit.subject
+  puts commit.message
+  commit_parents = commit.parents
+  if commit_parents.size > 0
+    parent_commit = commit_parents.get(0)
+    commit_tree = commit.tree
+    parent_tree = parent_commit.tree
+
+    diff = Ggit::Diff.new(repo, :old_tree => parent_tree,
+                          :new_tree => commit_tree, :options => nil)
+  end
+end
 
